c650ad7ef97ed8cb8949c3c47bcf07f9866092bf
Andrew Stacey <loopspace@mathforge.org>
2019-03-04 22:59:54 +0000

    Added enumeration to nodes to iterate over edges, fixed algorithms for traversal and path finding

--- OOGraph.cs
+++ OOGraph.cs
@@ -12,76 +12,52 @@ namespace MainProgram
 	    
 	    Node node;
 	    string label;
+	    int distance;
+	    List<Edge> path;
+	    Node current;
+	    string[] delimiters = { " ", "->", "-" };
+	    string[] edgelabel;
+	    List<Node> visited;
+	    List<string> labels = new List<string>();
 
 	    Console.WriteLine("Enter the graph's nodes:");
 	    label = Console.ReadLine();
 
 	    while (label != "")
 	    {
-		node = new Node(label);
-		graph.nodes.Add(node);
+		graph.AddNodeByLabel(label);
 		label = Console.ReadLine();
 	    }
 	    Console.WriteLine("Enter the graph's edges:");
 	    label = Console.ReadLine();
 	    
-	    string[] delimiters = { " ", "->", "-" };
-	    string[] edgelabel;
-	    Edge edge;
-	    Node start;
-	    Node end;
-	    
 	    while (label != "")
 	    {
 		edgelabel = label.Split(delimiters, System.StringSplitOptions.RemoveEmptyEntries);
 
-		start = graph.GetNodeByLabel(edgelabel[0]);
-		end = graph.GetNodeByLabel(edgelabel[1]);
-
-		if (start == null) {
-		    start = new Node(edgelabel[0]);
-		    graph.nodes.Add(start);
-		}
-
-		if (end == null) {
-		    end = new Node(edgelabel[1]);
-		    graph.nodes.Add(end);
-		}
-
-		edge = new Edge();
-		edge.start = start;
-		edge.end = end;
-		start.edges.Add(edge);
-		end.edges.Add(edge);
-
-		if (edgelabel.Length > 2)
-		{
-		    edge.weight = int.Parse(edgelabel[2]);
-		}
+		graph.AddEdgeBetween(edgelabel[0], edgelabel[1], int.Parse(edgelabel[2]));
 		
-		graph.edges.Add(edge);
 		label = Console.ReadLine();
 	    }
-
 	    
 	    Console.WriteLine("Enter the starting node for the searches:");
 	    label = Console.ReadLine();
 
-	    start = graph.GetNodeByLabel(label);
-	    List<Node> visited = graph.DepthFrom(start);
-	    List<string> labels = new List<string>();
+	    visited = graph.DepthFrom(label);
+	    labels = new List<string>();
 
 	    for (int i = 0; i < visited.Count; i++) {
 		node = visited[i];
-		labels.Add(node.contents);
+		labels.Add(node.Name);
 	    }
 	    Console.WriteLine("Depth first  : {0}", String.Join(", ", labels));
 
-	    visited = graph.BreadthFrom(start);
+	    visited = graph.BreadthFrom(label);
 	    labels = new List<string>();
+
 	    for (int i = 0; i < visited.Count; i++) {
 		node = visited[i];
-		labels.Add(node.contents);
+		labels.Add(node.Name);
 	    }
 	    Console.WriteLine("Breadth first: {0}", String.Join(", ", labels));
 
@@ -89,17 +65,17 @@ namespace MainProgram
 	    label = Console.ReadLine();
 	    edgelabel = label.Split(delimiters, System.StringSplitOptions.RemoveEmptyEntries);
 
-	    start = graph.GetNodeByLabel(edgelabel[0]);
-	    end = graph.GetNodeByLabel(edgelabel[1]);
 	    labels = new List<string>();
-	    int distance = 0;
-
-	    List<Edge> path = graph.PathBetween(start, end);
+	    distance = 0;
+	    path = graph.PathBetween(edgelabel[0], edgelabel[1]);
+	    current = graph.GetNodeByLabel(edgelabel[0]);
+	    labels.Add(current.Name);
 	    for (int i = path.Count - 1; i >= 0; i--) {
-		labels.Add(path[i].start.contents);
-		distance += path[i].weight;
+		labels.Add(path[i].GetOtherEnd(current).Name);
+		current = path[i].GetOtherEnd(current);
+		distance += path[i].Length;
 	    }
-	    labels.Add(path[0].end.contents);
+
 	    Console.WriteLine("Shortest path has length {0}: {1}", distance, String.Join(" -> ", labels));
 	    
 	}
@@ -109,24 +85,89 @@ namespace MainProgram
 namespace GraphStructures
 {
     
-    public class Node
+    public class Node: IEnumerable<Edge>
     {
-	public List<Edge> edges = new List<Edge>();
-	public bool marked { get; set; }
-	public int weight { get; set; }
-	public string contents { get; set; }
-	public Edge path { get; set; }
+	private List<Edge> Edges = new List<Edge>();
+	public bool Visited { get; set; }
+	public int Weight { get; private set; }
+	public string Name { get; private set; }
+	public Edge Path { get; private set; }
 
 	public Node (string c) {
-	    contents = c;
+	    Name = c;
 	}
 
-	public void VisitRecursively(ref List<Node> visited) {
-	    marked = true;
-	    visited.Add(this);
-	    for (int i = 0; i < edges.Count; i++) {
-		if (!edges[i].end.marked) {
-		    edges[i].end.VisitRecursively(ref visited);
+	public void Reset() {
+	    Visited = false;
+	    Weight = int.MaxValue;
+	    Path = null;
+	}
+
+	public void AddEdge(Edge e)
+	{
+	    if (! Edges.Contains(e))
+	    {
+		Edges.Add(e);
+	    }
+	}
+
+	public void UpdateWeight(Edge e)
+	{
+	    Node other = e.GetOtherEnd(this);
+	    if (other.Weight + e.Length < Weight)
+	    {
+		Weight = other.Weight + e.Length;
+		Path = e;
+	    }
+	}
+
+	public void ZeroWeight() {
+	    Weight = 0;
+	}
+
+	public void VisitRecursively(ref List<Node> v) {
+	    Node node;
+	    Visited = true;
+	    v.Add(this);
+	    for (int i = 0; i < Edges.Count; i++) {
+		node = Edges[i].GetOtherEnd(this);
+		if (!node.Visited) {
+		    node.VisitRecursively(ref v);
+		}
+	    }
+	}
+
+	public IEnumerator<Edge> GetEnumerator()
+	{
+	    for (int i = 0; i < Edges.Count; i++)
+	    {
+		yield return Edges[i];
+	    }
+	}
+
+	System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+	{
+	    return this.GetEnumerator();
+	}
+
+	IEnumerator<Edge> IEnumerable<Edge>.GetEnumerator()
+	{
+	    return GetEnumerator();
+	}
+
+	public IEnumerable<Edge> Forward
+	{
+	    get {
+		return this;
+	    }
+	}
+
+	public IEnumerable<Edge> Reverse
+	{
+	    get {
+		for (int i = Edges.Count - 1; i >= 0; i--)
+		{
+		    yield return Edges[i];
 		}
 	    }
 	}
@@ -135,56 +176,124 @@ namespace GraphStructures
 
     public class Edge
     {
-	public Node start {get; set;}
-	public Node end {get; set;}
-	public bool marked {get; set;}
-	public int weight {get; set;}
+	private List<Node> Ends = new List<Node>();
+	public int Length {get; private set;}
+
+	public Edge (Node s, Node e, int l)
+	{
+	    Ends.Add(s);
+	    Ends.Add(e);
+	    Length = l;
+	    s.AddEdge(this);
+	    e.AddEdge(this);
+	}
+
+	public Node GetOtherEnd(Node s)
+	{
+	    if (Ends[0] == s)
+	    {
+		return Ends[1];
+	    }
+	    else if (Ends[1] == s)
+	    {
+		return Ends[0];
+	    }
+	    else
+	    {
+		return null;
+	    }
+	}
     }
     
     public class Graph
     {
-	public List<Node> nodes = new List<Node>();
-	public List<Edge> edges = new List<Edge>();
-	
-	public Node GetNodeByLabel (string label) {
-	    foreach (Node node in nodes)
-	    {
-		if (node.contents == label) {
-		    return node;
-		}
-	    }
-	    return null;
-	}
+	private Dictionary<string,Node> nodes = new Dictionary<string,Node>();
+	private List<Edge> edges = new List<Edge>();
 
-	public bool ContainsNodeByLabel (string label) {
-	    foreach (Node node in nodes)
+	public bool AddNode(Node node)
+	{
+	    string label = node.Name;
+	    if (!nodes.ContainsKey(label))
 	    {
-		if (node.contents == label) {
-		    return true;
-		}
+		nodes.Add(label,node);
+		return true;
 	    }
 	    return false;
 	}
 	
-	public void ClearAll() {
-	    foreach (Node node in nodes)
+	public Node AddNodeByLabel(string label) {
+	    if (nodes.ContainsKey(label))
 	    {
-		node.marked = false;
-		node.weight = int.MaxValue;
-		node.path = null;
+		return null;
+	    }
+	    else
+	    {
+		Node node = new Node(label);
+		nodes.Add(label,node);
+		return node;
+	    }
+	}
+	
+	public Node GetNodeByLabel (string label) {
+	    if (nodes.ContainsKey(label))
+	    {
+		return nodes[label];
+	    }
+	    else
+	    {
+		return null;
 	    }
 	}
 
-	public List<Node> VisitAll() {
-	    ClearAll();
+	public bool ContainsNodeByLabel (string label) {
+	    return nodes.ContainsKey(label);
+	}
+
+	public Edge AddEdgeBetween (string s, string e, int l)
+	{
+	    Node sn;
+	    Node en;
+	    if (nodes.ContainsKey(s))
+	    {
+		sn = GetNodeByLabel(s);
+	    }
+	    else
+	    {
+		sn = new Node(s);
+	    }
+	    if (nodes.ContainsKey(e))
+	    {
+		en = GetNodeByLabel(e);
+	    }
+	    else
+	    {
+		en = new Node(e);
+	    }
+
+	    Edge edge = new Edge(sn,en,l);
+	    edges.Add(edge);
+	    return edge;
+	}
+	
+	public void ResetAll() {
+	    foreach (KeyValuePair<string,Node> entry in nodes)
+	    {
+		entry.Value.Reset();
+	    }
+	}
+
+	public List<Node> VisitAllFrom(string s) {
+	    Node start = GetNodeByLabel(s);
+	    ResetAll();
 	    List<Node> visits = new List<Node>();
-	    nodes[0].VisitRecursively(ref visits);
-	    ClearAll();
+	    start.VisitRecursively(ref visits);
+	    ResetAll();
 	    return visits;
 	}
 
-	public List<Node> DepthFrom(Node start) {
-	    ClearAll();
+	public List<Node> DepthFrom(string s) {
+	    Node start = GetNodeByLabel(s);
+	    ResetAll();
 	    List<Node> visits = new List<Node>();
 	    Stack<Node> stack = new Stack<Node>();
 	    stack.Push(start);
@@ -193,23 +302,26 @@ namespace GraphStructures
 	    while (stack.Count > 0) {
 		current = stack.Pop();
 
-		if (!current.marked)
+		if (!current.Visited)
 		{
 		    visits.Add(current);
-		    current.marked = true;
-		    for (int i = current.edges.Count - 1; i >= 0; i--) {
-			if (!current.edges[i].end.marked) {
-			    stack.Push(current.edges[i].end);
+		    current.Visited = true;
+		    foreach (Edge e in current.Reverse)
+		    {
+			if (!e.GetOtherEnd(current).Visited) {
+			    stack.Push(e.GetOtherEnd(current));
 			}
 		    }
 		}
 	    };
-	    ClearAll();
+	    ResetAll();
 	    return visits;
 	}
 
-	public List<Node> BreadthFrom(Node start) {
-	    ClearAll();
+	public List<Node> BreadthFrom(string s) {
+	    Node start = GetNodeByLabel(s);
+	    
+	    ResetAll();
 	    List<Node> visits = new List<Node>();
 	    Queue<Node> queue = new Queue<Node>();
 	    queue.Enqueue(start);
@@ -217,56 +329,63 @@ namespace GraphStructures
 	    
 	    while (queue.Count > 0) {
 		current = queue.Dequeue();
-		if (!current.marked)
+		if (!current.Visited)
 		{
 		    visits.Add(current);
-		    current.marked = true;
-		    for (int i = 0; i < current.edges.Count; i++) {
-			if (!current.edges[i].end.marked) {
-			    queue.Enqueue(current.edges[i].end);
+		    current.Visited = true;
+		    foreach (Edge e in current.Forward)
+		    {
+			if (!e.GetOtherEnd(current).Visited) {
+			    queue.Enqueue(e.GetOtherEnd(current));
 			}
 		    }
 		}
-	    };
-	    ClearAll();
+	    }
+	    ResetAll();
 	    return visits;
 	}
 
 	public Node FindNearest() {
 	    int weight = int.MaxValue;
-	    Node node = nodes[0];
-	    for (int i = 0; i < nodes.Count; i++) {
-		if (!nodes[i].marked && nodes[i].weight < weight)
+	    Node node = null;
+	    foreach (KeyValuePair<string,Node> entry in nodes) {
+		if (!entry.Value.Visited && entry.Value.Weight < weight)
 		{
-		    node = nodes[i];
-		    weight = nodes[i].weight;
+		    node = entry.Value;
+		    weight = entry.Value.Weight;
 		}
 	    }
 	    return node;
 	}
 
-	public List<Edge> PathBetween(Node start, Node end) {
-	    ClearAll();
+	public List<Edge> PathBetween(string s, string e) {
+	    Node start = GetNodeByLabel(s);
+	    Node end = GetNodeByLabel(e);
+	    
+	    ResetAll();
 	    List<Edge> path = new List<Edge>();
 	    Node current;
-	    start.weight = 0;
+	    Node other;
+	    start.ZeroWeight();
+	    current = FindNearest();
 
-	    for (int k = 0; k < nodes.Count; k++) {
-		current = FindNearest();
-		current.marked = true;
-		for (int i = 0; i < current.edges.Count; i++) {
-		    if (current.weight + current.edges[i].weight < current.edges[i].end.weight) {
-			current.edges[i].end.weight = current.weight + current.edges[i].weight;
-			current.edges[i].end.path = current.edges[i];
-		    }
+	    while (current != null)
+	    {
+		current.Visited = true;
+		foreach (Edge edge in current) {
+		    other = edge.GetOtherEnd(current);
+		    other.UpdateWeight(edge);
 		}
+
+		current = FindNearest();
 	    }
 	    current = end;
-	    while (current.path != null) {
-		path.Add(current.path);
-		current = current.path.start;
+	    while (current.Path != null) {
+		path.Add(current.Path);
+		current = current.Path.GetOtherEnd(current);
 	    }
-	    ClearAll();
+	    ResetAll();
+
 	    return path;
 	}
     }
